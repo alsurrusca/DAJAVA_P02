bccb2764fb1c081a89a4bee341df1280cad65912
alsurrusca <alissia.surrusca@gmail.com>
2022-03-09 20:57:56 +0100

    Final Version

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,124 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Read, count and make symptoms in order.
+ *
+ *
+ */
+
+
+
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+	private List<String> listSymptoms;
+	private Map<String, Integer> listSymptomsCount = new HashMap<String, Integer>();
+	private Map<String, Integer> listOrderSymptoms;
+
+
+	public void start()  {
+
+		this.getSymptoms();
+		this.countSymptoms();
+		this.orderSymptoms();
+		this.saveSymptoms();
 	}
-}
+
+
+	private void getSymptoms() {
+
+		/**
+		 * @see ReadSymptomDataFromFile
+		 */
+
+		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("E:\\Etude\\Java\\Formation Java\\Projet 2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master\\Project02Eclipse\\symptoms.txt");
+		listSymptoms = readSymptomDataFromFile.getSymptoms();
+
+
+
+	}
+
+
+	private void countSymptoms() {
+
+		/**
+		 *
+		 * @param listSymtoms
+		 * @return list which count all symptom's occurrences
+		 *
+		 */
+
+		for(String symptom : listSymptoms) {
+
+		if (listSymptomsCount.containsKey(symptom)) {
+			listSymptomsCount.put(symptom, (listSymptomsCount.get(symptom) + 1));
+		}
+			else {
+				listSymptomsCount.put(symptom, 1);
+			}
+
+
+		}
+
+
+	}
+
+
+
+
+	public void orderSymptoms()  {
+
+		/**
+		 * Create a set of the same elements contained in the hash map
+		 * Order by key
+		 * @return LinkedHashMap
+		 *
+		 */
+		listOrderSymptoms = listSymptomsCount.entrySet()
+				.stream()
+				.sorted(Map.Entry.comparingByKey())
+				.collect(Collectors.toMap(
+						Map.Entry :: getKey,
+						Map.Entry :: getValue,
+						(oldValue, newValue) -> oldValue, LinkedHashMap::new
+
+
+				));
+
+	}
+
+
+
+
+
+
+	private void saveSymptoms() {
+
+		/**
+		 *
+		 * Call WriteSymptomsFromData
+		 *
+		 * @see WriteSymptomsFromData
+		 *
+		 * @return void
+		 *
+		 */
+
+
+			WriteSymptomsFromData writeSymptomsFromData = new WriteSymptomsFromData();
+			writeSymptomsFromData.saveSymptoms(listOrderSymptoms);
+
+
+	}
+
+
+	}
+
+
+
